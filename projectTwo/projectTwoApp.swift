bbebc490df970ce52e0cc870a3578182f5448a73
griffin muir <gmuir@lcs.on.ca>
2023-02-07 20:11:58 -0500

    Initial Commit

---
+++ projectTwo/projectTwoApp.swift
@@ -0,0 +1,17 @@
+//
+//  projectTwoApp.swift
+//  projectTwo
+//
+//  Created by Samantha Stewart on 2023-02-07.
+//
+
+import SwiftUI
+
+@main
+struct projectTwoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
